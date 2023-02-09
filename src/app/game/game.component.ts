85b74937f57ea6b91c704f2f1fb37cdeaa8a3da5
Niclas Michel <81393421+nicmicdev@users.noreply.github.com>
2023-02-09 10:57:44 +0100

    add player and player component added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
@@ -7,16 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  
-  constructor() {}
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog) { }
 
 
   ngOnInit(): void {
-    
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      console.log('New card: ' + this.currentCard);
+      console.log('Game: ', this.game);
+      
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000); 
+    }
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+     
+    });
+
+    dialogRef.afterClosed().subscribe((name:string) => {
+      this.game.players.push(name);
+    });
+  }
+
+
 
 }
